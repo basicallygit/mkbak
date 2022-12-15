eb2a9334b3a1878d90f714686fc589edf443b183
basicallygit <91993321+basicallygit@users.noreply.github.com>
2022-12-15 23:39:46 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::fs;
+use std::env::args;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+
+    if args.len() <= 1 {
+        println!("Usage: mkbak [-f]? <files> [-f]?");
+        return;
+    }
+
+    let force: bool; //force overwrite of already existing .bak files
+    let startindex: usize;
+    let mut endindex: usize = args.len();
+
+    if &args[1] == "-f" {
+        force = true;
+        startindex = 2;
+    }
+    else if &args[args.len() - 1] == "-f" {
+        force = true;
+        startindex = 1;
+        endindex = args.len() - 1;
+    }
+    else {
+        force = false;
+        startindex = 1;
+    }
+
+    for file in &args[startindex..endindex] {
+        match make_bak(file, force) {
+            Ok(_) => println!("[mkbak] {} -> {}.bak", file, file),
+            Err(e) => println!("{}: {}", file, e),
+        }
+    }
+
+}
+
+fn make_bak(file: &str, force: bool) -> Result<(), std::io::Error> {
+    let mut bak = file.to_string();
+    bak.push_str(".bak");
+
+    if fs::metadata(&bak).is_ok() { //if .bak file already exists
+        if !force { //if force is not enabled log error, else continue to overwrite
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::AlreadyExists,
+                format!(" {}: File exists, skipping... (use -f to force)", bak)
+            ));
+        }
+    }
+
+    fs::copy(file, bak)?;
+    Ok(())
+}
